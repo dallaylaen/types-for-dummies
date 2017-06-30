1acfc2509897c62803bab19bc01c8b9b60f34f8e
Konstantin S. Uvarin <khedin@gmail.com>
2017-06-30 03:55:30 +0300

    EMU WIP Try new Todo hierarchy now (not working)

--- parse-stream.scala
+++ parse-stream.scala
@@ -6,8 +6,6 @@ import scala.util.matching.Regex.Match;
 class ParseError(s: String) extends Exception("PASRE: "+s) {
 }
 
-
-
 class ParseTape (init: String, iline: Int = 0, ioffset: Int = 1) {
     var content = init
     var line    = iline
@@ -26,30 +24,22 @@ class ParseTape (init: String, iline: Int = 0, ioffset: Int = 1) {
             }
         }
     }
-    def where(): String = { "line "+line+" offset "+offset }
-    def whereLast(): String = { "line "+line+" offset "+prev }
-}
+    def nonempty(): Boolean = { content.length > 0 }
 
-class ParseContext {
-    def merge( other: ParseContext ) {
-        throw new ParseError("unimplemented");
-    }
+    def whereNow(): String = { "line "+line+" offset "+offset }
+    def where(): String = { "line "+line+" offset "+prev }
 }
 
 
-class ParseOutcome {
-    
-}
-
-class ParseExpect {
-    type Handler = (Match,ParseContext)=>ParseOutcome
+class Rules[Ctx] {
+    type Handler = (Match,Ctx)=>Todo[Ctx]
     var rules : List[Pair[Regex,Handler]] = List()
-    def addRule( r: Regex, todo: Handler ): ParseExpect = {
+    def addRule( r: Regex, todo: Handler ): Rules[Ctx] = {
         rules = rules :+ Pair(r, todo)
         this
     }
 
-    def grabPrefix( src: ParseTape, ctx: ParseContext ): ParseOutcome = {
+    def grabPrefix( src: ParseTape, ctx: Ctx ): Todo[Ctx] = {
         for (pair <- rules) {
             src.grabPrefix(pair._1) match {
                 case None => {}
@@ -58,8 +48,45 @@ class ParseExpect {
         }
         throw new ParseError("No rule found for '"+src+"'"+" at "+src.where);
     }
-    
 }
+
+class Todo[Ctx] {
+}
+
+class TodoFwd[Ctx](rules: Rules[Ctx], ctx: Ctx) extends Todo[Ctx] {
+    def grabPrefix(src: ParseTape): Todo[Ctx] = {
+        rules.grabPrefix(src, ctx)
+    }
+}
+
+class TodoDescend[Ctx](rules: Rules[Ctx], ctx: Ctx, imerge: Ctx=>Todo[Ctx])
+        extends TodoFwd[Ctx](rules, ctx) {
+    def merge(ctx: Ctx): Todo[Ctx] = { imerge(ctx) }
+}
+
+class TodoAscend[Ctx](ctx: Ctx) {
+    def context(): Ctx = { ctx }
+}
+
+
+
+
+
+class ParserCycle[Ctx] {
+    def descend(src: ParseTape, start: Todo[Ctx]): Ctx = {
+        throw new Exception ("not done");
+    }
+    def parseLine( rules: Rules[Ctx], ctx: Ctx, src: ParseTape ): Ctx = {
+        descend(src, new TodoFwd(rules, ctx))
+    }
+}
+
+// ------------------------------------
+
+
+
+// ------------------------------------
+
 
 object Smoke {
     def main (arg: Array[String]): Unit = {
@@ -67,16 +94,16 @@ object Smoke {
 
         println ("at "+tape.offset+": "+tape.content)
 
-        var ctx = new ParseContext
+        var ctx = ""
 
-        var chain = new ParseExpect()
+        var chain = new Rules[String]()
         chain.addRule( """\s*baz\w+""".r, (x, y) => { 
-                println( "found "+x+" at "+ tape.whereLast )
-                new ParseOutcome()
+                println( "found "+x+" at "+ tape.where )
+                new Todo[String]()
         } );
         chain.addRule( """\s*foo\w+""".r, (x, y) => { 
-                println( "found "+x+" at "+ tape.whereLast )
-                new ParseOutcome()
+                println( "found "+x+" at "+ tape.where )
+                new Todo[String]()
         } );
 
         println("pass1");
